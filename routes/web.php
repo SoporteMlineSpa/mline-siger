c74abf7a6663f6dc2d4061b0fd640693c4c4ea00
Jose Monagas <jose_tadeomong@hotmail.com>
2019-12-12 17:14:27 -0300

    Ordenar rutas

--- routes/web.php
+++ routes/web.php
@@ -15,44 +15,73 @@
 
 Auth::routes();
 
-Route::get('/notificaciones', 'ShowNotification')->name('notifications');
-
-Route::resource('empresas', 'EmpresaController');
-Route::resource('centros', 'CentroController');
-
-Route::group(['prefix' => 'cliente'], function() {
-
-    Route::get('/', 'HomeController@index')->name('cliente.home');
-
-    Route::group(['prefix' => 'pedidos'], function() {
-
-        Route::get('crear', 'RequerimientoController@create')->name('requerimientos.create');
-        Route::post('store', 'RequerimientoController@store')->name('requerimientos.store');
-        Route::get('editar/{requerimiento}', 'RequerimientoController@edit')->name('requerimientos.edit');
-
-        Route::get('validar-pedidos', 'RequerimientoController@validarPedidos')->name('pedidos.validar');
-        Route::get('{centro}/{estado?}', 'RequerimientoController@showCentro')->name('pedidos.centro');
-        Route::get('/', 'RequerimientoController@index')->name('requerimientos.index');
-
-        Route::post('aceptar', 'RequerimientoController@aceptar')->name('pedidos.aceptar');
-        Route::post('rechazar', 'RequerimientoController@rechazar')->name('pedidos.rechazar');
-        Route::post('aceptar-todos', 'RequerimientoController@aceptarTodos')->name('pedidos.aceptarTodos');
-    });
-
+Route::get('/', function() {
+    if (Auth::user()->userable instanceof \App\CompassRole) {
+        return redirect()->to('/compass/');
+    } else {
+        return redirect()->to('/cliente/');
+    }
 });
 
-Route::group(['prefix' => 'compass'], function() {
+Route::group(['middleware' => 'auth'], function() {
 
-    Route::get('/', 'HomeController@index')->name('compass.home');
+    // Notificaciones
+    Route::get('/notificaciones', 'ShowNotification')->name('notifications');
 
-    Route::resource('productos', 'ProductoController');
+    // Global CRUDS
+    Route::resource('empresas', 'EmpresaController')->except([
+        'show'
+    ]);
+    Route::resource('centros', 'CentroController')->except([
+        'show'
+    ]);
 
-    Route::group(['prefix' => 'pedidos'], function() {
-        Route::get('/', 'RequerimientoController@index')->name('compass.pedidos.index');
-        Route::get('verificar', 'RequerimientoController@verificar')->name('compass.pedidos.verificar');
+    // Rutas del Cliente
+    Route::group(['prefix' => 'cliente'], function() {
 
-        Route::get('armar', 'RequerimientoController@indexCajas')->name('compass.pedidos.cajasIndex');
-        Route::get('armar/{requerimientoId}', 'RequerimientoController@show')->name('compass.pedidos.show');
+        Route::get('/', 'HomeController@index')->name('cliente.home');
+
+        Route::group(['prefix' => 'pedidos'], function() {
+
+            Route::get('crear', 'RequerimientoController@create')->name('requerimientos.create');
+            Route::post('store', 'RequerimientoController@store')->name('requerimientos.store');
+            Route::get('editar/{requerimiento}', 'RequerimientoController@edit')->name('requerimientos.edit');
+
+            Route::get('validar-pedidos', 'RequerimientoController@validarPedidos')->name('pedidos.validar');
+            Route::get('{centro}/{estado?}', 'RequerimientoController@showCentro')->name('pedidos.centro');
+            Route::get('/', 'RequerimientoController@index')->name('requerimientos.index');
+
+            Route::post('aceptar', 'RequerimientoController@aceptar')->name('pedidos.aceptar');
+            Route::post('rechazar', 'RequerimientoController@rechazar')->name('pedidos.rechazar');
+            Route::post('aceptar-todos', 'RequerimientoController@aceptarTodos')->name('pedidos.aceptarTodos');
+        });
+
     });
 
+    // Rutas de Compass
+    Route::group(['prefix' => 'compass'], function() {
+
+        Route::get('/', 'HomeController@index')->name('compass.home');
+
+        Route::resource('productos', 'ProductoController');
+        Route::resource('holdings', 'HoldingController')->except([
+            'show'
+        ]);
+
+        Route::get('usuarios/{tipo}', 'UserController@index')->name('usuarios.index');
+
+        Route::resource('usuarios', 'UserController')->except([
+            'index'
+        ]);
+
+        Route::group(['prefix' => 'pedidos'], function() {
+
+            Route::get('/', 'RequerimientoController@index')->name('compass.pedidos.index');
+            Route::get('verificar', 'RequerimientoController@verificar')->name('compass.pedidos.verificar');
+
+            Route::get('armar', 'RequerimientoController@indexCajas')->name('compass.pedidos.cajasIndex');
+            Route::get('armar/{requerimientoId}', 'RequerimientoController@show')->name('compass.pedidos.show');
+        });
+
+    });
 });
